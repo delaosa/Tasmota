8c67c676167a6f62a5772839219371a6506343d9
delaosa <delaosa@gmail.com>
2019-12-23 23:02:09 +0100

    changed mqtt timeout to 60sec

--- lib/PubSubClient-EspEasy-2.7.12/src/PubSubClient.h
+++ lib/PubSubClient-EspEasy-2.7.12/src/PubSubClient.h
@@ -31,7 +31,7 @@
 // Keepalive timeout for default MQTT Broker is 10s
 #ifndef MQTT_KEEPALIVE
 //#define MQTT_KEEPALIVE 10
-#define MQTT_KEEPALIVE 30  // Tasmota v6.5.0.14 enabling AWS-iot
+#define MQTT_KEEPALIVE 60  // Tasmota v6.5.0.14 enabling AWS-iot
 #endif
 
 // MQTT_SOCKET_TIMEOUT: socket timeout interval in Seconds
